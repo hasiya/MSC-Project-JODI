cbb5d01ea050841ce1281b5e74eff3a7058fdf20
Ali Colville <hasith002@yahoo.com>
2016-06-28 16:54:37 +0100

    Formatting

--- readers/CSV.py
+++ readers/CSV.py
@@ -6,14 +6,6 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-# def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
-#     csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
-#     for row in csv_reader:
-#         yield [unicode(cell, 'utf-8') for cell in row]
-#
-# def csv_unireader(f, encoding="utf-8"):
-#     for row in csv.reader(codecs.iterencode(codecs.iterdecode(f, encoding), "utf-8")):
-#         yield [e.decode("utf-8") for e in row]
 
 def read(string):
     try:
@@ -23,9 +15,9 @@ def read(string):
         Data_dict = []
         i = 1
         for line in reader:
-            if (i == 1):
+            if i == 1:
                 headers = line
-                i = i + 1
+                i += 1
 
             else:
                 line_as_dict = {}
@@ -33,7 +25,7 @@ def read(string):
                     line_as_dict[h] = line[headers.index(h)]
                     # print(h, ":",line[h])
                 Data_dict.append(line_as_dict)
-                i = i + 1
+                i += 1
         return Data_dict
     except IOError as e:
         print(e)
